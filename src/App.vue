d577ed71c43f26048080030c05d58f70c3638677
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-11-19 19:49:31 +0100

    Style login screen in a fast way

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,13 @@ export default {
 </script>
 
 <style>
-* { margin: 0; padding: 0; box-sizing: border-box; }
+html, body{ margin:0; padding:0; height:100%; width:100%; }
 body { font: 13px Helvetica, Arial; }
+#app {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
+  <div id="login-screen">
+    <img src="~assets/img/logo-white.png"/>
+    <h1>Welcome to Qowala!</h1>
+    <p>For starters, use your credentials to log in your preferred messaging app</p>
     <form action="" v-on:submit.prevent="sendCredentials">
-      <input type="text" name="email" placeholder="Email" v-model="email" />
-      <input type="password" name="password" v-model="password"/>
-      <input id="submit" type="submit" />
+      <label for="email">
+        Email address
+      </label>
+      <input type="text" name="email" placeholder="Email address" autofocus v-model="email" />
+      <label for="password">
+        Password
+      </label>
+      <input type="password" name="password" placeholder="Password" v-model="password"/>
+      <input id="submit" type="submit" value="Log in"/>
     </form>
-    <div v-if="loading">Login...</div>
-    <div v-if="failed">Login failed!</div>
+    <div id="loading" v-if="loading">Login...</div>
+    <div id="loading-failed" v-if="failed">Login failed!</div>
   </div>
 </template>
 
@@ -52,21 +61,83 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+#login-screen h1,
+#login-screen p,
+#login-screen #loading,
+#login-screen #loading-failed {
+  text-align: center;
+  color: white;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+#login-screen h1 {
+  margin-bottom: 20px;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+#login-screen p {
+  margin-bottom: 20px;
 }
 
-a {
-  color: #42b983;
+#login-screen {
+  background-color: #3AD78D;
+  padding: 40px;
+  height: 100%;
 }
+
+#login-screen img {
+  width: 100px;
+  margin: 20px auto 40px;
+  display: block;
+}
+
+#login-screen label,
+#login-screen input[type="text"],
+#login-screen input[type="password"],
+#login-screen input[type="text"]::placeholder,
+#login-screen input[type="password"]::placeholder {
+  color: white;
+}
+
+#login-screen label,
+#login-screen input[type="text"]::placeholder,
+#login-screen input[type="password"]::placeholder {
+  text-transform: uppercase;
+  font-size: 0.8em;
+}
+
+#login-screen label {
+  display: block;
+  width: 130px;
+  margin: 10px auto 0;
+  position: relative;
+  right: 75px;
+}
+
+#login-screen form input {
+  display: block;
+  margin: 5px auto 0;
+  width: 260px;
+  background-color: #3AD78D;
+  height: 30px;
+  border-radius: 5px;
+  border: solid 2px white;
+}
+
+#login-screen input[type="text"],
+#login-screen input[type="password"] {
+  font-size: 0.9em;
+  padding-left: 20px;
+}
+
+#login-screen input[type="submit"] {
+  background-color: white;
+  color: #3AD78D;
+  width: 100px;
+  margin-top: 40px;
+}
+
+#login-screen #loading,
+#login-screen #loading-failed {
+  margin-top: 20px;
+}
+
 </style>
