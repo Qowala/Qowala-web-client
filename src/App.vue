78e2a5d046a3a93eb0f8e9ff249d483266235544
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-11-23 22:33:07 +0100

    Fix scrolling

--- src/App.vue
+++ src/App.vue
@@ -23,10 +23,7 @@ export default {
 html, body{ margin:0; padding:0; height:100%; width:100%; }
 body { font: 13px Helvetica, Arial; }
 #app {
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
+	height: 100%;
+	overflow-x: hidden;
 }
 </style>
