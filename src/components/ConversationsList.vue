5b2fff8ae4920529626ac13ac13330963be7e65c
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-11-28 22:27:48 +0100

    Style notification when new message

--- src/components/ConversationsList.vue
+++ src/components/ConversationsList.vue
@@ -56,25 +56,16 @@ export default {
       this.messageInput = '';
     },
     notifyMe: function notifyMe(msg) {
-      const notifMsg = 'Qowala: ' + msg;
-      // Let's check whether notification permissions have already been granted
-      if (Notification.permission === "granted") {
-        // If it's okay let's create a notification
-        var notification = new Notification(notifMsg);
+      const options = {
+        body: msg,
+        icon: '/static/img/favicon.png'
       }
 
-      // Otherwise, we need to ask the user for permission
-      else if (Notification.permission !== 'denied') {
-        Notification.requestPermission(function requestPermission (permission) {
-          // If the user accepts, let's create a notification
-          if (permission === "granted") {
-            var notification = new Notification(notifMsg);
-          }
-        });
-      }
+      Notification.requestPermission().then(function(result) {
+        console.log(result);
+      });
 
-      // At last, if the user has denied notifications, and you
-      // want to be respectful there is no need to bother them any more.
+      const notification = new Notification('New message:', options);
     }
   },
 	sockets: {
