bb6b520df1984b293e862fdf5179e03352e80c31
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-12-03 15:05:54 +0100

    First Service worker POC with front stand-alone
    Following this tutorial:
    https://developers.google.com/web/fundamentals/getting-started/codelabs/push-notifications/#sending_push_messages

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,65 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    {{ this.subscription }}
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      subscription: {},
+    };
+  },
+  created: function () {
+
+    var endpoint;
+    var key;
+    var authSecret;
+
+    // Register a Service Worker.
+    navigator.serviceWorker.register('static/service-worker.js')
+    .then(function(registration) {
+    console.log('youhou, registered');
+      // Use the PushManager to get the user's subscription to the push service.
+      return registration.pushManager.getSubscription()
+      .then(function(subscription) {
+        // If a subscription was found, return it.
+        if (subscription) {
+          return subscription;
+        }
+
+        function urlBase64ToUint8Array(base64String) {
+          const padding = '='.repeat((4 - base64String.length % 4) % 4);
+          const base64 = (base64String + padding)
+            .replace(/\-/g, '+')
+            .replace(/_/g, '/');
+
+          const rawData = window.atob(base64);
+          const outputArray = new Uint8Array(rawData.length);
+
+          for (let i = 0; i < rawData.length; ++i) {
+            outputArray[i] = rawData.charCodeAt(i);
+          }
+          return outputArray;
+        }
+
+        const convertedVapidKey = urlBase64ToUint8Array('BExs199noJRJuvBYq45Y5VJuMUMRW9H0927NPGtLKRvx9kkIASEANmBgOlsoEXm6Nyxc2TKTEtOtwei7X8-rkq0');
+        // Otherwise, subscribe the user (userVisibleOnly allows to specify that we don't plan to
+        // send notifications that don't have a visible effect for the user).
+        return registration.pushManager.subscribe({ userVisibleOnly: true,
+        applicationServerKey: convertedVapidKey});
+      });
+    }).then(function(subscription) {
+      // Retrieve the user's public key.
+      console.log('retrieving user key...');
+
+      this.subscription = subscription;
+      console.log('subcription added');
+    }.bind(this));
+  },
 	sockets: {
 		'err': function(err) {
 			console.log('An error occured: ', err);
