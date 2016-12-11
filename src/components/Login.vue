f17fcf27914e861adc5dc302045752faef86c289
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-12-11 22:32:58 +0100

    Subscribe to service worker only after login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -41,6 +41,72 @@ export default {
       this.loading = true;
       this.email = '';
       this.password = '';
+    },
+    subscribeSw: function(token) {
+      if ('serviceWorker' in navigator && 'PushManager' in window) {
+        console.log('Service Worker and Push is supported');
+
+        const applicationServerPublicKey = 'BP5DsTGZtwA4cqV1vgTggxDdy-6fBY5I--_3fPCHAir9kUS5rR_vAzHKc0htxxGx_sFz-02liGu8PgoZr9DEr3Y';
+
+        // Register a Service Worker.
+        navigator.serviceWorker.register('static/service-worker.js')
+        .then(function(registration) {
+        console.log('youhou, registered');
+          // Use the PushManager to get the user's subscription to the push service.
+          return registration.pushManager.getSubscription()
+          .then(function(subscription) {
+            // If a subscription was found, return it.
+            if (subscription) {
+              console.log('already a subscription: ', subscription);
+              return subscription;
+            }
+
+            function urlBase64ToUint8Array(base64String) {
+              const padding = '='.repeat((4 - base64String.length % 4) % 4);
+              const base64 = (base64String + padding)
+                .replace(/\-/g, '+')
+                .replace(/_/g, '/');
+
+              const rawData = window.atob(base64);
+              const outputArray = new Uint8Array(rawData.length);
+
+              for (let i = 0; i < rawData.length; ++i) {
+                outputArray[i] = rawData.charCodeAt(i);
+              }
+              return outputArray;
+            }
+
+            const convertedVapidKey = urlBase64ToUint8Array(applicationServerPublicKey);
+            // Otherwise, subscribe the user (userVisibleOnly allows to specify that we don't plan to
+            // send notifications that don't have a visible effect for the user).
+            return registration.pushManager.subscribe({ userVisibleOnly: true,
+            applicationServerKey: convertedVapidKey});
+          });
+        }).then(function(subscription) {
+          // Retrieve the user's public key.
+          console.log('retrieving user key...');
+
+          this.subscription = subscription;
+          console.log('subscription added: ', subscription);
+
+          const payload = {
+            subscription: subscription,
+            notification: {
+              title: 'Bob',
+              body: 'heeeey it should work',
+              icon: '/static/img/favicon.png'
+            },
+            token: token
+          };
+
+          this.$socket.emit('swSendNotification', payload);
+        }.bind(this))
+        .catch(function(error) {
+          console.error('Service Worker Error: ', error);
+        });
+      } else {
+        console.warn('Push messaging is not supported');
+      }
     }
   },
   sockets: {
@@ -48,6 +114,8 @@ export default {
       this.loading = false;
 			localStorage.setItem('qowala-token', token);
       console.log('redirecting to conversations list');
+      // Subsribe to service worker to get notifications
+      this.subscribeSw(token);
       this.$router.push('/');
     },
     'login failed': function () {
